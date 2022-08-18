370b89556f0867027e5839bf24eb3d4d468595c6
Juan Carlos <104734299+Juankkar@users.noreply.github.com>
2022-08-18 18:32:04 +0100

    Update pig_pol.R

--- AFV/practicas_camp/scripts/pig_pol.R
+++ AFV/practicas_camp/scripts/pig_pol.R
@@ -3,13 +3,14 @@
 #########################      Pigmentos polifenoles           #################
 ################################################################################
 
-library(tidyverse)
 library(ggthemes)
-library(DescTools)
 library(readxl)
 library(rgl)
 library(tidytext)
 library(xlsx)
+
+# Este link provee un script donde tengo una funciones interesantes hechas por mí
+source("https://raw.githubusercontent.com/Juankkar/mis_cosas/main/funciones_propias/inferencia.R")
 
 url1 <- "https://raw.githubusercontent.com/Juankkar/cuarto_carrera/main/AFV/practicas_camp/bases_datos/pig_polifenoles_pino.csv"
 pig_pol_pino <- read_csv(url1)
@@ -43,18 +44,11 @@ pig_pol_pino %>%
 
 #-------------------------- Diferenicias NBI? ---------------------------#
 h_adulta <- pig_pol_pino %>% filter(hoja %in% "Adulta")
-tapply(h_adulta$nbi, h_adulta$exposicion, shapiro.test)          # Se cumple la normaliddad
-LeveneTest(nbi~exposicion, data = h_adulta, center = "mean")     # Se cumple la homocedasticidad
-t.test(nbi~exposicion, data = h_adulta)                          # p > 0.05
-
+tw.groups(h_adulta, nbi, "nbi", exposicion, "Sol", "Sombra")     # p > 0.05 
 h_joven <- pig_pol_pino %>% filter(hoja %in% "Joven")
-tapply(h_joven$nbi, h_joven$exposicion, shapiro.test)            # Se cumple la normaliddad
-LeveneTest(nbi~exposicion, data = h_joven, center="mean")        # Se cumple la homocedasticidad
-t.test(nbi~exposicion, data = h_joven)                           # p > 0.05
-
+tw.groups(h_joven, nbi, "nbi", exposicion, "Sol", "Sombra")     # p > 0.05 
 h_senescente <- pig_pol_pino %>% filter(hoja %in% "Senescente")
-tapply(h_senescente$nbi, h_senescente$exposicion, shapiro.test)  # No se cumple la normaliddad
-wilcox.test(nbi~exposicion, data = h_senescente)                 # p > 0.05
+tw.groups(h_senescente, nbi, "nbi", exposicion, "Sol", "Sombra")     # p > 0.05 
 #### Clorofila Pino
 
 pig_pol_pino %>% 
@@ -84,16 +78,9 @@ pig_pol_pino %>%
   scale_fill_manual(values = c("white", "gray"))
 #----------------------- Diferencias Chl Pino? -----------------------------#
 
-tapply(h_adulta$chl, h_adulta$exposicion, shapiro.test)          # Se cumple la normaliddad
-LeveneTest(chl~exposicion, data = h_adulta, center = "mean")     # Se cumple la homocedasticidad
-t.test(chl~exposicion, data = h_adulta)                          # p > 0.05
-
-tapply(h_joven$chl, h_joven$exposicion, shapiro.test)            # Se cumple la normaliddad
-LeveneTest(chl~exposicion, data = h_joven, center="mean")        # No se cumple la homocedasticidad
-t.test(chl~exposicion, data = h_joven, var.eq=FALSE)             # p > 0.05
-
-tapply(h_senescente$chl, h_senescente$exposicion, shapiro.test)  # No se cumple la normaliddad
-wilcox.test(chl~exposicion, data = h_senescente)                 # p > 0.05
+tw.groups(h_adulta, chl, "chl", exposicion, "Sol", "Sombra")         # p > 0.05 
+tw.groups(h_joven, chl, "chl", exposicion, "Sol", "Sombra")          # p > 0.05 
+tw.groups(h_senescente, chl, "chl", exposicion, "Sol", "Sombra")     # p > 0.05 
 
 # Flavonoides Pino
 
@@ -123,17 +110,10 @@ pig_pol_pino %>%
         legend.text = element_text(size = 11)) +
   scale_fill_manual(values = c("white", "gray"))
 #----------------- Diferencias significativas Flavonoides Pino?----------------#
-tapply(h_adulta$flav, h_adulta$exposicion, shapiro.test)          # No se cumple la normaliddad
-wilcox.test(flav~exposicion, data = h_adulta)                     # p > 0.05
 
-tapply(h_joven$flav, h_joven$exposicion, shapiro.test)            # Se cumple la normaliddad
-LeveneTest(flav~exposicion, data = h_joven, center="mean")        # Se cumple la homocedasticidad
-t.test(flav~exposicion, data = h_joven)                           # p > 0.05
-
-tapply(h_senescente$flav, h_senescente$exposicion, shapiro.test)  # Se cumple la normaliddad
-LeveneTest(flav~exposicion, data = h_senescente, center="mean")   # Se cumple la homocedasticidad
-t.test(flav~exposicion, data = h_senescente)                      # p > 0.05
-
+tw.groups(h_adulta, flav, "flav", exposicion, "Sol", "Sombra")    # p > 0.05 
+tw.groups(h_joven, flav, "flav", exposicion, "Sol", "Sombra")     # p > 0.05 
+tw.groups(h_senescente, flav, "flav", exposicion, "Sol", "Sombra")# p > 0.05 
 
 # Antocianinas
 
@@ -166,18 +146,10 @@ pig_pol_pino %>%
   scale_fill_manual(values = c("white", "gray"))
 
 #------------------ Diferencias significativas antocianinas Pino?--------------#
-tapply(h_adulta$antocianinas, h_adulta$exposicion, shapiro.test)          # Se cumple la normaliddad
-LeveneTest(antocianinas~exposicion, data = h_adulta, center="mean")       # No se cumple la homocedasticidad
-t.test(antocianinas~exposicion, data = h_adulta)                          # p > 0.05
 
-tapply(h_joven$antocianinas, h_joven$exposicion, shapiro.test)            # Se cumple la normaliddad
-LeveneTest(antocianinas~exposicion, data = h_joven, center="mean")        # Se cumple la homocedasticidad
-t.test(antocianinas~exposicion, data = h_joven)                           # p < 0.05*
-
-tapply(h_senescente$antocianinas, h_senescente$exposicion, shapiro.test)  # Se cumple la normaliddad
-LeveneTest(antocianinas~exposicion, data = h_senescente, center="mean")   # Se cumple la homocedasticidad
-t.test(antocianinas~exposicion, data = h_senescente)                      # p > 0.05
-
+tw.groups(h_adulta, antocianinas, "antocianinas", exposicion, "Sol", "Sombra")    # p > 0.05 
+tw.groups(h_joven, antocianinas, "antocianinas", exposicion, "Sol", "Sombra")     # p < 0.05*
+tw.groups(h_senescente, antocianinas, "antocianinas", exposicion, "Sol", "Sombra")# p > 0.05 
 
 ####### Análisis multivariante PCA de Pigmentos y polifenoles de Pino  #########
 
@@ -229,7 +201,7 @@ games_howell_test(PC2~hoja, data = df_comp_prin_pino)                           
 
 #### Como con la tercera componentes se explica cerca del 99% de la varianza vamos a 
 #### echar un vistazo mediante un grafico 3D
-
+library(rgl)
 df_cp_pino_colores <- df_comp_prin_pino %>% 
   mutate(colores=case_when(hoja == "Joven" ~ "forestgreen",
                            hoja == "Adulta" ~"red",
@@ -245,7 +217,7 @@ plot3d(x=df_cp_pino_colores$PC1, y=df_cp_pino_colores$PC2, z=df_cp_pino_colores$
 
 url2 <- "https://raw.githubusercontent.com/Juankkar/cuarto_carrera/main/AFV/practicas_camp/bases_datos/pig_polifenoles_rosalillo.csv"
 pig_pol_rosalillo <- read_csv(url2) 
- 
+
 #### NBI Rosalillo
 
 pig_pol_rosalillo %>% 
@@ -274,9 +246,8 @@ pig_pol_rosalillo %>%
         legend.text = element_text(size = 11)) +
   scale_fill_manual(values = c("white", "gray"))
 #-------- Diferencias entre NBI Haz y envés / sombra y Sol del Rosalillo? -----#
-tapply(pig_pol_rosalillo$nbi, pig_pol_rosalillo$h_e, shapiro.test)                     # Se cumple Normalidad
-LeveneTest(nbi~h_e, data = pig_pol_rosalillo, center="mean")                           # Se cumple homocedasticidad
-anova_pp_rosa.nbi <- aov(nbi~h_e, data = pig_pol_rosalillo);summary(anova_pp_rosa.nbi) # p > 0.05 
+
+tw.groups(pig_pol_rosalillo, nbi, "nbi", h_e, "Envés", "Haz")# p > 0.05 
 
 # Clorofilas rosalillo
 
@@ -306,9 +277,8 @@ pig_pol_rosalillo %>%
         legend.text = element_text(size = 11)) +
   scale_fill_manual(values = c("white", "gray"))
 #------------------------ Diferencias Clorofilas rosalillo? -------------------#
-tapply(pig_pol_rosalillo$chl, pig_pol_rosalillo$haz_enves, shapiro.test)                     # Se cumple Normalidad
-LeveneTest(chl~h_e, data = pig_pol_rosalillo, center="mean")                           # No se cumple homocedasticidad
-welch_anova_test(chl~h_e, data = pig_pol_rosalillo)                                    # p > 0.05
+
+tw.groups(pig_pol_rosalillo, chl, "chl", h_e, "Envés", "Haz")# p > 0.05 
 
 # Flavonoides rosalillo
 
@@ -338,8 +308,8 @@ pig_pol_rosalillo %>%
         legend.text = element_text(size = 11)) +
   scale_fill_manual(values = c("white", "gray"))
 #----------------------- Diferencias flavinas Rosalillo? ----------------------#
-tapply(pig_pol_rosalillo$flav, pig_pol_rosalillo$h_e, shapiro.test)     # No se cumple Normalidad
-kruskal.test(flav~h_e, data = pig_pol_rosalillo)                        # p > 0.05
+
+tw.groups(pig_pol_rosalillo, flav, "flav", h_e, "Envés", "Haz")# p > 0.05 
 
 # Antocianinas
 
@@ -369,11 +339,10 @@ pig_pol_rosalillo %>%
         legend.text = element_text(size = 11)) +
   scale_fill_manual(values = c("white", "gray"))
 #------------------- Diferencias antocianinas Rosalillo? ----------------------#
-tapply(pig_pol_rosalillo$antocianinas, pig_pol_rosalillo$h_e, shapiro.test)                       # Se cumple Normalidad
-LeveneTest(antocianinas~h_e, data = pig_pol_rosalillo, center="mean")                             # Se cumple la Homocedasticidad
-anova_pp_rosa_anto <- aov(antocianinas~haz_enves, data = pig_pol_rosalillo);summary(anova_pp_rosa.nbi)  # p > 0.05
 
-                                                           #           haz sombra-envés sombra; haz sombra-haz-sol   
+tw.groups(pig_pol_rosalillo, antocianinas, "antocianinas", h_e, "Envés", "Haz")# p > 0.05 
+
+#           haz sombra-envés sombra; haz sombra-haz-sol   
 
 #######################################################################################
 ####################### PCA DE LAS ESPECIES DE ROSALILLO y Pino #######################
@@ -439,6 +408,4 @@ wilcox.test(PC1~especie.2, data = df_pc_total) # p < 0.05 en la PC1, existen dif
 tapply(df_pc_total$PC2, df_pc_total$especie.2, shapiro.test)   # p > 0.05 en ambos, los datos son normales
 LeveneTest(PC2~especie.2, data = df_pc_total, center = "mean") # p > 0.05 los datos son homocedásticos 
 t.test(PC1~especie.2, data = df_pc_total)                      # p < 0.05 en la PC2 también, existen diferencias significativas
-
- 
 

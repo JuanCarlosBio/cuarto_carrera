34034ccd3dc840a0790800325890c55667adbed6
Juan Carlos <104734299+Juankkar@users.noreply.github.com>
2022-09-13 12:48:42 +0100

    Update tenebrios.R

--- FAA/tenebrios/scripts_tenebrios/scripts/tenebrios.R
+++ FAA/tenebrios/scripts_tenebrios/scripts/tenebrios.R
@@ -17,24 +17,11 @@ library(ggthemes)
 library(glue)
 
 #### Nuestros resultados para el grupo 4 de Oscuridad.
-
-result_o4 <- read_excel("practica 1 aiai/tenebrios.xlsx", # Puedes importar fácilmente la tabla de excel en el "Import Dataset" de la derecha, si te has descargado el csv de mi cuenta :)
-                        sheet = "resultados_nuestros") %>% 
-  mutate(dia=seq(0,77,7))
-
-url1 <- "https://raw.githubusercontent.com/Juankkar/tenebrios_faa/main/bases_datos/tenebrios.csv"
-result_o4 <- read.csv(url1, sep = ";"); colnames(result_o4) <- c("semana",        # Semana de estudio
-                                                                 "masa.corporal", # valores anotados cada semana del peso de las larvas
-                                                                 "tasa.crec.s",   # Lo que crecen los tenebrios al semana tras semana
-                                                                 "tasa.crec.d")   # # Lo que crecen los tenebrios día a día (en promedio de los 7 días de la semana realmente)
-result_o4 <- result_o4 %>% mutate(masa.corporal = str_replace(masa.corporal, pattern = ",", replacement = "."),
-                                  tasa.crec.s = str_replace(tasa.crec.s, pattern = ",", replacement = "."),
-                                  tasa.crec.d = str_replace(tasa.crec.d, pattern = ",", replacement = "."),
-                                  dia = seq(0,77,7))
-
-result_o4$masa.corporal <- as.numeric(result_o4$masa.corporal)
-result_o4$tasa.crec.d <- as.numeric(result_o4$tasa.crec.d)
-result_o4$tasa.crec.s <- as.numeric(result_o4$tasa.crec.s)
+url1 <- "https://raw.githubusercontent.com/Juankkar/cuarto_carrera/main/FAA/tenebrios/scripts_tenebrios/datos/tenebrios.csv"
+result_o4 <- read_csv(url1);colnames(result_o4) <- c("semana",        # Semana de estudio
+                                                     "masa.corporal", # valores anotados cada semana del peso de las larvas
+                                                     "tasa.crec.s",   # Lo que crecen los tenebrios al semana tras semana
+                                                     "tasa.crec.d")   # # Lo que crecen los tenebrios día a día (en promedio de los 7 días de la semana realmente)
 
 
 # Comencemos con lo más básico, un estudio del aumento de la masa corporal y posteriormenete de la tasa de crecimeinto.
@@ -43,12 +30,12 @@ result_o4$tasa.crec.s <- as.numeric(result_o4$tasa.crec.s)
 
 masa_tasa_o4 <- result_o4[,c("semana","masa.corporal","tasa.crec.s")] %>%       # Filtramos las variables que nos interesa en este caso 
   pivot_longer(cols = -semana,names_to = "masa_tasa", values_to = "valores")    # (y lo llamamos de esta manera, no se cuan inexperto 
-                                                                                # eres, pero en R puedes asignar un valor a un objeto con
-                                                                                # "<-" o "=", te recomiendo en este caso acostumbrarte al 
-                                                                                # primero en R). Pues ya tendríamos un data frame con los 
-                                                                                # valores de masa y tasa en una columna, y otra que nos diga 
-                                                                                # si es la tasa o masa, igual si vienes de excel estás un poco 
-                                                                                # perdido, pero ahora verás por qué he hecho esto en el gráfico 
+                                                        # eres, pero en R puedes asignar un valor a un objeto con
+                                                        # "<-" o "=", te recomiendo en este caso acostumbrarte al 
+                                                        # primero en R). Pues ya tendríamos un data frame con los 
+                                                        # valores de masa y tasa en una columna, y otra que nos diga 
+                                                        # si es la tasa o masa, igual si vienes de excel estás un poco 
+                                                        # perdido, pero ahora verás por qué he hecho esto en el gráfico 
 
 
 # Para el gráfico usaremos ggplot2, uno de los mejores paquetes para gráficos,
@@ -59,22 +46,22 @@ masa_tasa_o4 <- result_o4[,c("semana","masa.corporal","tasa.crec.s")] %>%       
 # la interpretación de los gráfico lo que dejo en sus manos, yo ya la hice en su 
 # momento xd, no es mi intención en estos scripts
 
-p1 <- masa_tasa_o4 %>% 
+masa_tasa_o4 %>% 
   mutate(masa_tasa = factor(masa_tasa,
                             levels = c("masa.corporal", "tasa.crec.s"),             # De esta manera cambio los nombres a los grupos
-                            labels = c("Masa Corporal", "Tasa Crecimiento"))) %>%    # que los otros no eran estéticos
+                            labels = c("Masa Corporal (g)", "Tasa Crecimiento (g)"))) %>%    # que los otros no eran estéticos
   ggplot(aes(semana, valores, col=masa_tasa)) +
-  geom_line(size=1) +
-  geom_point(size=1.75) +
+  geom_line(size=1, show.legend = F) +
+  geom_point(size=1.75, show.legend = F) +
+  facet_wrap(~masa_tasa, ncol=1, scales = "free",
+             strip.position = "left") +
   labs(title = "Variación masa corporal y tasa de crecimiento",
        subtitle = "Fisiología Animal Aplicada ULL, Grupo 4 Oscuridad",
        x="Semanas",
-       y="Gramos",
+       y=NULL,
        col="Variación") +
-  scale_x_continuous(limits = c(1,12),
-                     breaks = seq(1,12,1)) +
-  scale_y_continuous(limits = c(0,.18),
-                     breaks = seq(0,.18,.03)) +
+  scale_x_continuous(limits = c(0,13),
+                     breaks = seq(0,12,1)) +
   scale_color_manual(values = c("black","orange")) +
   theme(
     panel.background = element_blank(),
@@ -83,18 +70,19 @@ p1 <- masa_tasa_o4 %>%
     plot.subtitle = element_text(size = 12, face = "bold", hjust = .5),
     axis.title = element_text(size = 15, face = "bold"),
     axis.text = element_text(size = 10),
-    legend.position = c(.25,.8),
-    legend.background = element_rect(color = "black"),
-    legend.key = element_rect(fill="white"),
-    legend.title = element_text(hjust = .5, size=13, face = "bold"),
-    legend.text = element_text(size = 12)
+    legend.position = "bottom",
+    legend.text = element_text(size = 12),
+    strip.placement = "outside",
+    strip.background = element_blank(),
+    strip.text = element_text(face="bold", size = 11)
   )
+
 # Los reultados anteriores de son los valores promedio de cada semana, el cuál es 
 # el que hay que subir al aula virtual. En el siguiente data frame tengo los valores 
 # de cada tenebrio con el paso de las semanas, para ver qué nos permite esto con 
 # respecto al anterior
 
-url2 <- "https://raw.githubusercontent.com/Juankkar/tenebrios_faa/main/bases_datos/df_o4.csv"
+url2 <- "https://raw.githubusercontent.com/Juankkar/cuarto_carrera/main/FAA/tenebrios/scripts_tenebrios/datos/df_o4.csv"
 df_o4 <- read.csv(url2, sep = ";") 
 colnames(df_o4) <- c("semana1","semana2","semana3","semana4","semana5","semana6",
                      "semana7","semana8","semana9","semana10","semana11")
@@ -102,8 +90,8 @@ df_o4_2 <- df_o4 %>%
   pivot_longer(cols=starts_with("semana"),
                names_to = "semana_name", 
                values_to = "masa.corporal") %>% 
-               mutate(masa.corporal = str_replace(masa.corporal, pattern = ",", replacement = "."),
-                      masa.corporal = as.numeric(masa.corporal)) %>% 
+  mutate(masa.corporal = str_replace(masa.corporal, pattern = ",", replacement = "."),
+         masa.corporal = as.numeric(masa.corporal)) %>% 
   group_by(semana_name) %>% 
   summarise(media_mc = mean(masa.corporal,na.rm=T),
             sd = sd(masa.corporal,na.rm = T)) %>%  # de esta manera obtenemos la desviación típica
@@ -129,8 +117,8 @@ df_o4_2 <- df_o4 %>%
 
 na <- data.frame(na=NA)
 tasa_crec <- data.frame(tasa.crec.s=df_o4_2[2:11,2]-df_o4_2[1:10,2]) # Con esta formula sacamos la tasa de crecimiento
-                                                                     # no podía añadirla en un mutate(), ya que como el vector
-                                                                     # presenta 2 valores menos, no se puede meter. Seguramente
+# no podía añadirla en un mutate(), ya que como el vector
+# presenta 2 valores menos, no se puede meter. Seguramente
 df_o4_completo <- df_o4_2 %>%                                        # haya formas de hacerlo más smooth como la geometría, pero  
   mutate(tasa.crec.s = c(tasa_crec$media_mc,na$na),                  # sigo verde :(. Pero bueno, lo importante al final en 
          tasa.crec.d = tasa.crec.s/7,                                # programación es llegar al resultado :)
@@ -159,7 +147,7 @@ row.names(coeficientes) <- c("Intercepto", "Pendiente")
 colnames(coeficientes) <- c("Resultados")
 abline <- c(glue(coeficientes[1,]),         # Estas líneas de código con glue() es 
             glue(coeficientes[2,]))         # para que si cambio los números de arriba, 
-                                            # no tenga que volver a cambiarlos en el gráfico
+# no tenga que volver a cambiarlos en el gráfico
 text_num <- round(as.numeric(abline),3)
 text <- c(glue("Masa orporal = {text_num[2]}·Semana + {text_num[1]}")) 
 
@@ -194,7 +182,7 @@ df_o4_completo %>%
     legend.key = element_rect(fill="white"),
     legend.title = element_text(hjust = .5, size=13, face = "bold"),
     legend.text = element_text(size = 12)
-    )
+  )
 
 
 
@@ -203,9 +191,6 @@ df_o4_completo %>%
 cor.test(x=df_o4_completo$media_mc,
          y=df_o4_completo$tasa.crec.s,
          method = "pearson")           # p > 0.05, las variables no están correlacionadas linealmente, estimación = -0.04, nula correlación lineal negativa
-
-
-
 
 
 
